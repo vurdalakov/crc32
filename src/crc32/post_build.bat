96d4492338f1ab4b406f4cbd6472903663efc78e
vurdalakov <vurdalakov@localhost>
2013-04-26 12:59:59 +0300

    Build environment updated to 64-bit version of Windows

--- src/crc32/post_build.bat
+++ src/crc32/post_build.bat
@@ -3,7 +3,7 @@
 set ConfigurationName=%1
 set TargetDir=%2
 
-set ilmerge="C:\Program Files\Microsoft\ILMerge\ILMerge.exe"
+set ilmerge="C:\Program Files (x86)\Microsoft\ILMerge\ILMerge.exe"
 
 if %ConfigurationName%==Release (
     %ilmerge% /out:%TargetDir%crc32.exe %TargetDir%crc32exe.exe %TargetDir%vurdalakovdotnet.dll
